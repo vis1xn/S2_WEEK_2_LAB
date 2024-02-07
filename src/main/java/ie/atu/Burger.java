29130e3c5385c2e2723148c19bf9fe12b6b744f3
Tyrese <g00421432@atu.ie>
2024-02-07 16:31:50 +0000

    Added new classes along with code

--- src/main/java/ie/atu/Burger.java
+++ src/main/java/ie/atu/Burger.java
@@ -1,4 +1,29 @@
 package ie.atu;
 
-public class Burger {
+public class Burger implements  MenuItem {
+    private String name;
+    private double price;
+    private  String ingredients;
+
+    public  Burger(String name, double price, String ingredients){
+        this.name = name;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+    @Override
+    public  String getName(){
+        return name;
+    }
+
+    @Override
+    public double getPrice(){
+        return price;
+    }
+
+    @Override
+    public  String getDescription(){
+        return ingredients;
+    }
+
 }
+
