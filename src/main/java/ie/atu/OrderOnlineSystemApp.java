0ca260be270482cf46cb408567e7c34e30883003
Tyrese <g00421432@atu.ie>
2024-02-07 16:44:26 +0000

    added new class "O0SA" along with new code

---
+++ src/main/java/ie/atu/OrderOnlineSystemApp.java
@@ -0,0 +1,24 @@
+package ie.atu;
+
+import java.awt.*;
+
+public class OrderOnlineSystemApp {
+
+    public static void main(String[] args){
+
+        MenuItem[] menuItems = new MenuItem[]{
+                //add items
+        };
+
+        for (MenuItem menuItem : menuItems) {
+            // name and description for each
+        }
+
+        double totalCost = 0;
+        for (MenuItem menuItem : menuItems) {
+            //totalCost of all menuItems prices in an order
+        }
+        System.out.println("Total Cost: €" + totalCost);
+
+    }
+}
