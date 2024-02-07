9af401090a8885ef587e4215020794582874f81e
Tyrese <g00421432@atu.ie>
2024-02-07 17:28:30 +0000

    finshed System App, added and completed code for Dessert class

--- src/main/java/ie/atu/OrderOnlineSystemApp.java
+++ src/main/java/ie/atu/OrderOnlineSystemApp.java
@@ -7,16 +7,21 @@ public class OrderOnlineSystemApp {
     public static void main(String[] args){
 
         MenuItem[] menuItems = new MenuItem[]{
-                //add items
+                new Burger("Classic Burger", 7.99, "Beef patty, lettuce, tomato, cheese, pickles"),
+                new Pizza("Pepperoni Pizza", 13.99, "pepperoni, cheese, tomato sauce"),
+                new Salad("Caeser Salad", 4.99, "Romaine lettuce, tomatoes, croutons, 1000 Ilse dressing"),
+                new Dessert("Chocolate Cake", 15.69, "Eggs, Milk, Chocolate powder, Yeast")
         };
 
         for (MenuItem menuItem : menuItems) {
-            // name and description for each
+            System.out.println("Name: " + menuItem.getName());
+            System.out.println("Descripton: " + menuItem.getDescription());
+            System.out.println("Price: €" + menuItem.getPrice() + "\n");
         }
 
         double totalCost = 0;
         for (MenuItem menuItem : menuItems) {
-            //totalCost of all menuItems prices in an order
+        totalCost += menuItem.getPrice();
         }
         System.out.println("Total Cost: €" + totalCost);
 
